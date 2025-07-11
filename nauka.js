103b167c156d5af07a0689a7746126e2a9509efe
pradnietyka <92676074+pradnietyka@users.noreply.github.com>
2025-07-11 13:36:59 +0200

    Add files via upload

---
+++ nauka.js
@@ -0,0 +1,42 @@
+
+let a = 10;
+console.log(a);
+
+const imie = "Ania";
+console.log(imie);
+
+let n = -5;
+if (n > 0) {
+  console.log("Dodatnia");
+  } else if (n === 0 ) 
+  console.log ("Liczba rowna 0");
+  else 
+  console.log("Ujemna")
+ 
+
+ console.log(typeof a);
+ console.log(typeof imie);
+ console.log(typeof n);
+
+ console.log(5 == "5")
+ console.log(5 === "5")
+
+ let i = 1;
+ i++ ;
+ console.log(i)
+ console.log(i)
+
+ let name = "michał"
+ console.log('Mam na imie ' + name + '!') 
+
+ let liczba = 6;
+ if (liczba % 2 === 0) {
+    console.log("Parzysta");
+ }
+
+ let temp = 29;
+ if (temp < 30) {
+  console.log("Ciepło")
+ } else if (temp === 30) {
+    console.log("Gorąco")
+ } else console.log ("UPAŁ")
